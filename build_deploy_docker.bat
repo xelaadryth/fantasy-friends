87ba55e88059780906627224f0ab0fd1fb3d6d78
Xelaadryth <Xelaadryth@users.noreply.github.com>
2016-08-31 01:23:50 -0700

    Running local docker deploy needs to modify GIN_MODE after the environment variables file.

--- build_deploy_docker.bat
+++ build_deploy_docker.bat
@@ -1,7 +1,6 @@
 ::Build statically-linked binary for alpine linux
 set GOOS=linux
 set GOARCH=amd64
-set GIN_MODE=debug
 go build -o alpine_binary .
 
 deploy_docker.bat
--- deploy_docker.bat
+++ deploy_docker.bat
@@ -1,3 +1,3 @@
 ::Build Docker image to container and run it
 docker build -t fantasy_image .
-docker run --rm -it --env-file .env --publish 8080:8080 --name fantasy-container fantasy_image
+docker run --rm -it --env-file .env -e "GIN_MODE=debug" --publish 8080:8080 --name fantasy-container fantasy_image
